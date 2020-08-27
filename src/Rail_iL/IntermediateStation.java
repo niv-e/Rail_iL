8aa1e6b487802449822184e8c9bc476acc97ac89
nadeen-maree <61875414+nadeen-maree@users.noreply.github.com>
2020-08-27 21:46:23 +0300

    Update IntermediateStation.java
    added method isCorrectExpectedStopTime(), that check if the expected stop time is correct

--- src/Rail_iL/IntermediateStation.java
+++ src/Rail_iL/IntermediateStation.java
@@ -8,6 +8,17 @@ public class IntermediateStation extends Station {
         System.out.println("please enter expected stop time: ");
         expectedStopTime.setTime();
     }
+    
+    	Ride r = new Ride();
+	public boolean isCorrectExpectedStopTime(Ride r) {
+		if(r.getDepartureTime().getHours() > expectedStopTime.getHours() || r.getDestinationTime().getHours() < expectedStopTime.getHours()) {
+			return false;
+		} else if(((r.getDepartureTime().getHours() == expectedStopTime.getHours())&&(r.getDepartureTime().getMinutes() > expectedStopTime.getMinutes())) 
+				||((r.getDestinationTime().getHours() == expectedStopTime.getHours()) && (r.getDestinationTime().getMinutes() < expectedStopTime.getMinutes()))) {
+			return false;
+		}
+		return true;
+	}
 
     public String toString(){
         StringBuilder sb = new StringBuilder();
