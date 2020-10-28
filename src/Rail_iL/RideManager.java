4743c77c8132a44b18e46795e5fc1d2e1185ebb7
nadeen-maree <61875414+nadeen-maree@users.noreply.github.com>
2020-10-28 20:15:09 +0200

    Update RideManager.java

--- src/Rail_iL/RideManager.java
+++ src/Rail_iL/RideManager.java
@@ -1,12 +1,18 @@
+package Rail_il;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class RideManager {
-    ArrayList<Ride> allRides;
+	ArrayList<Ride> allRides;
 
-    public RideManager(){
-        allRides =  new ArrayList<Ride>();
-    }
+	public RideManager(){
+		allRides =  new ArrayList<Ride>();
+	}
 
 	public void showRelevantRides(String departureName , String destinationName , String time){
 		Clock c = new Clock();
@@ -22,18 +28,63 @@ public class RideManager {
 		}
 	}
 
-    public void addRide (Ride ride){
-        allRides.add(ride);
-        Collections.sort(allRides , new SortRideByDepartureTime());
-    }
+	public void addRide (Ride ride){
+		allRides.add(ride);
+		Collections.sort(allRides , new SortRideByDepartureTime());
+	}
 
-    public String toString(){
-        StringBuilder sb = new StringBuilder();
-        int counter =1;
-        for ( Ride r : allRides) {
-            sb.append("\nride " + counter++ +": " + r.toString() +"\n");
-        }
-        return sb.toString();
-    }
+	public void saveToFile(String f) throws FileNotFoundException {
+		File file = new File(f);
+		PrintWriter pw = new PrintWriter(file);
+		pw.write(toString());
+
+		pw.close();
+	}
+
+	public static void readFile(File f) throws FileNotFoundException {
+		try { 
+			Scanner scan = new Scanner(new File("C:\\Users\\WINDOWS 10 PRO\\eclipse-workspace\\Rail_IL\\src\\Rail_il\\Rides details"));
+			RideManager rideManager = new RideManager();
+			Ride r1 = new Ride();
+			while(scan.hasNextLine()) {
+				scan.nextLine();scan.nextLine();
+				scan.next();scan.next();
+				r1.departure.setStationName(scan.next()); //departure name
+				scan.next();scan.next();
+				r1.departureTime.setTime(scan.next()); //departure time
+				scan.next();scan.next();
+				r1.destination.setStationName(scan.next()); //destination name
+				scan.next();scan.next();
+				r1.destinationTime.setTime(scan.next()); //destination time
+				int num = scan.nextInt();
+				scan.nextLine();
+				rideManager.addRide(r1);
+				ArrayList<IntermediateStation> allIntermediateStations = new ArrayList<IntermediateStation>();
+				IntermediateStation is = new IntermediateStation(null);
+				for (int i = 0; i < num; i++) {
+					String name  = scan.next().trim();
+					is.setStationName(name.substring(0,name.length()-1));
+					scan.next();scan.next();scan.next();
+					is.setTime(scan.next());
+					allIntermediateStations.add(is);	
+				}
+
+			}
+			scan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+	}
+
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		int counter =1;
+		for ( Ride r : allRides) {
+			sb.append("\nride " + counter++ +": " + r.toString() +"\n");
+		}
+		return sb.toString();
+	}
 
 }
