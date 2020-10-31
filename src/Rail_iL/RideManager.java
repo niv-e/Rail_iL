97e0c24a5ead1848cd2a03139a7a4ab3fe9ca522
nadeen-maree <61875414+nadeen-maree@users.noreply.github.com>
2020-10-31 02:01:38 +0200

    Update RideManager.java

--- src/Rail_iL/RideManager.java
+++ src/Rail_iL/RideManager.java
@@ -42,22 +42,22 @@ public class RideManager {
 
 	public void readFile(File f) throws FileNotFoundException {
 		try { 
-			Scanner scan = new Scanner(new File("C:\\Users\\WINDOWS 10 PRO\\eclipse-workspace\\Rail_IL\\src\\Rail_il\\Rides details")); // The location should change in another computer
-
+			Scanner scan = new Scanner(new File("C:\\Users\\User\\eclipse-workspace\\Rail-IL_User\\src\\Rail_il\\Rides details"));
+			String str;
 			while(scan.hasNextLine()) {
-				Ride r = new Ride();
+				Ride r = new Ride(); 
 				scan.nextLine();scan.nextLine();
 				scan.next();scan.next();
-				r.departure.setStationName(scan.next()); //departure name
+				r.departure.setStationName(scan.nextLine().trim()); //departure name
 				scan.next();scan.next();
 				r.departureTime.setTime(scan.next()); //departure time
 				scan.next();scan.next();
-				r.destination.setStationName(scan.next()); //destination name
+				r.destination.setStationName(scan.nextLine().trim()); //destination name
 				scan.next();scan.next();
 				r.destinationTime.setTime(scan.next()); //destination time
 				int num = scan.nextInt();
 				scan.nextLine();scan.nextLine();
-				IntermediateStation is = new IntermediateStation("toCheck");
+				IntermediateStation is = new IntermediateStation("tocheck");
 				if(num ==0)
 					scan.nextLine();
 				for (int i = 0; i < num; i++) {
